be6c98dbb19c71c0129a7c47ab7a6a46de072c3b
Cpapa97 <cpapa97@gmail.com>
2018-12-02 23:29:37 -0600

    Setting up repo for packaging

---
+++ labelops/__init__.py
@@ -0,0 +1,5 @@
+from .version import __version__
+
+__author__ = "Christos Papadopoulos at Baylor College of Medicine"
+__date__ = "Dec 2, 2018"
+__all__ = ['__author__', '__version__', 'label_ops']
---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from os import path
+import sys
+
+# Is this necessary?
+# if sys.version_info < (3, 6):
+#     sys.exit('LabelOps is only guaranteed to be supported on Python 3.6 or higher')
+
+here = path.abspath(path.dirname(__file__))
+
+long_description = "A package to handle specific operations on neuronal meshes with labeled data, including compressing and decompressing those labels."
+
+# read in version number
+with open(path.join(here, 'labelops', 'version.py')) as f:
+    exec(f.read())
+
+with open(path.join(here, 'requirements.txt')) as f:
+    requirements = f.read().split()
+
+setup(
+    name='datajoint',
+    version=__version__,
+    description="Operates on labeled neuronal meshes.",
+    long_description=long_description,
+    author='Christos Papadopoulos',
+    author_email='cpapa97@gmail.com',
+    license="GNU LGPL",
+    url='https://github.com/Cpapa97/LabelOps,
+    keywords='functional connectomics',
+    packages=find_packages(['labelops']),
+    install_requires=requirements,
+)
